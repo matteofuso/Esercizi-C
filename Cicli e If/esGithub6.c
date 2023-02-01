703625d0ec9e56211588244772eb058c1a25041c
Matteo <matteo.fuso@iisviolamarchesini.edu.it>
2023-02-01 11:38:55 +0100

    Esercizio banca, es5, es6

---
+++ Cicli e If/esGithub6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+    Progettare un algoritmo che effettui le seguenti operazioni:
+    • legga da tastiera una coppia di valori interi A e B con A<B;
+    • continui a leggere da tastiera una serie di valori interi, terminando
+    quando il valore letto è al di fuori dell’intervallo [A, B];
+    • conteggi la media dei valori letti;
+    • prima di terminare, stampi il valore calcolato.
+*/
+
+int main(int argc, char *argv[])
+{
+    float a, b, tmp, numero = 0, somma = 0, media;
+    int i = 0;
+    printf("Inserisci l'intervallo separato da un-:\n");
+    scanf("%f-%f", &a, &b);
+    if (a > b)
+    {
+        tmp = b;
+        b = a;
+        a = tmp;
+    }
+    do
+    {
+        somma += numero;
+        printf("Inserisci un numero: ");
+        scanf("%f", &numero);
+        i++;
+    } while (numero > a && numero < b);
+    i--;
+    media = somma / i;
+    printf("La media dei numeri compresi nell'intervallo %.2f-%.2f è %.2f\n", a, b, media);
+    return 0;
+}
