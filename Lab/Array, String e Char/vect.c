5b6dcd294eb08d66a83db53cb946cf0154b55aca
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2023-04-04 21:43:17 +0200

    Fix vect ex

--- Lab/Array, String e Char/vect.c
+++ Lab/Array, String e Char/vect.c
@@ -7,10 +7,14 @@
 */
 
 #include <stdio.h>
+#include <limits.h>
 
 int main(int argc, char *argv[])
 {
-    int N, i, maxPari[2], maxDispari[2], minPari[2], minDispari[2];
+    int N, i, maxPari[2], maxDispari[2], minPari[2], minDispari[2]; // Elemento in 0 è il numero, in 1 è l'indice
+    maxPari[1] = maxDispari[1] = -1;
+    maxPari[0] = maxDispari[0] = INT_MIN;
+    minPari[0] = minDispari[0] = INT_MAX;
     do
     {
         printf("Inserisci quanti elementi vuoi nel vettore: ");
@@ -21,60 +25,46 @@ int main(int argc, char *argv[])
     {
         printf("Inserisci il %d° numero: ", i + 1);
         scanf("%d", vect + i);
-    }
-    maxPari[1] = -1;
-    maxDispari[1] = -1;
-    minPari[1] = -1;
-    minDispari[1] = -1;
-    for (i = 1; i < N; i++)
-    {
-        if (N % 2 == 0)
+        if (vect[i] % 2 == 0)
         {
-            if (maxPari[1] == -1)
+            if (vect[i] > maxPari[0])
             {
                 maxPari[0] = vect[i];
                 maxPari[1] = i;
+            }
+            if (vect[i] < minPari[0])
+            {
                 minPari[0] = vect[i];
                 minPari[1] = i;
-            }
-            else
-            {
-                if (vect[i] > maxPari[0])
-                {
-                    maxPari[0] = vect[i];
-                    maxPari[1] = i;
-                }
-                else if (vect[i] < minPari[0])
-                {
-                    minPari[0] = vect[i];
-                    minPari[1] = i;
-                }
             }
         }
         else
         {
-            if (maxDispari[1] == -1)
+            if (vect[i] > maxDispari[0])
             {
                 maxDispari[0] = vect[i];
                 maxDispari[1] = i;
-                minDispari[0] = vect[i];
-                minDispari[1] = i;
             }
-            if (vect[i] > maxDispari[0])
-            {
-                minPari[0] = vect[i];
-                minPari[1] = i;
-            }
-            else if (vect[i] < minDispari[0])
+            if (vect[i] < minDispari[0])
             {
                 minDispari[0] = vect[i];
                 minDispari[1] = i;
             }
         }
     }
-    printf("Il numero pari minore è %d e si trova alla posizione %d", minPari[0], minPari[1]);
-    printf("Il numero pari maggiore è %d e si trova alla posizione %d", maxPari[0], maxPari[1]);
-    printf("Il numero dispari minore è %d e si trova alla posizione %d", minDispari[0], minDispari[1]);
-    printf("Il numero dispari maggiore è %d e si trova alla posizione %d", maxDispari[0], maxDispari[1]);
+    if (maxPari[1] != -1)
+    {
+        printf("Il numero pari minore è %d e si trova alla posizione %d\n", minPari[0], minPari[1]);
+        printf("Il numero pari maggiore è %d e si trova alla posizione %d\n", maxPari[0], maxPari[1]);
+    }
+    else
+    {
+        printf("Non ci sono numeri pari\n");
+    }
+    if (maxDispari[1] != -1)
+    {
+        printf("Il numero dispari minore è %d e si trova alla posizione %d\n", minDispari[0], minDispari[1]);
+        printf("Il numero dispari maggiore è %d e si trova alla posizione %d\n", maxDispari[0], maxDispari[1]);
+    }
     return 0;
 }
