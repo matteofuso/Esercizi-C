3f3b87f49ecf76cb13e675f08b71adfbafe3e402
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2023-08-11 21:23:05 +0200

    Esercizi Iterazioni

---
+++ Home/Esercizi/fibonacci.c
@@ -0,0 +1,17 @@
+/*
+    Calcolare la Successione di Fibonacci
+    In matematica, la successione di Fibonacci è una successione di numeri interi in cui
+    ciascun numero è la somma dei due precedenti, ad eccezione dei primi due, che sono 0 e 1.
+    La serie di fibonacci può essere usate per approsimare la conversione tra miglia e km.
+    Prendiamo per esempio 5 e 8, 8km = 5mi.
+
+    Sapendo questo chiedi all'utente l'unità di misura di partenza e successivamente trova il numero della serie
+    più vicino e stampa la conversione.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ Home/Esercizi/pi.c
@@ -0,0 +1,25 @@
+/*
+    Calcolare il Pi Greco:
+    Creare un menù che ci permetta di calcolare il pi greco con diverse formule:
+    1. Formula di Leibniz
+        pi/4 = 1/1 - 1/3 + 1/5 - 1/7 + 1/9 - ...
+        Per calcolare il pi greco basterà moltiplicare quindi tutto per 4
+        pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - ...
+        Da notare come il denominatore aumenta sempre di due e che si alternano somma e sottrazione
+    2. Formula di Nilakantha
+        pi - 3 = 1/(1*2*3) - 1/(2*3*5) + 1/(3*4*7) - 1/(4*5*9)
+        Da cui si ricava
+        pi = 3 + 1/(1*2*3) - 1/(2*3*5) + 1/(3*4*7) - 1/(4*5*9)
+        Da notare come al denominatore, il primo numero sia uguale a quello centrale della frazione precedente, 
+        seguito da n+1 e da n+i. Si nota inoltre l'alternarsi di somma e sottrazione
+    Chiedere all'utente quante volte eseguire il ciclo
+
+    Infine, dopo aver chiesto all'utente il raggio di un cerchio, calcolane l'area e ed il perimetro.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
