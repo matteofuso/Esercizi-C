05a131569bf8907aeed1f44149ce4498892f4973
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2023-08-14 13:50:04 +0200

    Soluzioni

--- Home/Esercizi/fibonacci.c
+++ Home/Esercizi/fibonacci.c
@@ -10,8 +10,48 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+    int p = 0, s = 1, n;
+    char unita[2] = "km";
+    // Chiedo l'unità di misura di partenza
+    do
+    {
+        printf("Inserisci l'unità di misura di partenza (km/mi): ");
+        scanf("%s", unita);
+    } while (strcmp("km", unita) != 0 && strcmp("mi", unita) != 0);
+    // Chiedo il numero di km o mi
+    do
+    {
+        printf("Inserisci il numero di %s: ", unita);
+        scanf("%d", &n);
+    } while (n < 1);
+    // Trovo dove n è compreso
+    while (p < n && s < n)
+    {
+        int tmp = p + s;
+        p = s;
+        s = tmp;
+    }
+    // Trovo il numero più vicino e lo assegno a p
+    if (n - p > s - n)
+    {
+        int tmp = p + s;
+        p = s;
+        s = tmp;
+    }
+    // Stampo la risposta
+    if (strcmp("mi", unita) == 0)
+    {
+        // Da miglia a chilometri, prendo il successivo
+        printf("%d %s sono circa %d km\n", n, unita, s);
+    }
+    else
+    {
+        // Da chilometri a miglia, prendo il precedente
+        printf("%d %s sono circa %d mi\n", n, unita, s - p);
+    }
     return 0;
 }
--- Home/Esercizi/pi2.c
+++ Home/Esercizi/pi2.c
@@ -21,8 +21,8 @@
 
 int main(int argc, char *argv[])
 {
-    int scelta, iterazioni;
-    double pi, raggio;
+    int scelta, iterazioni, n = 1, d, segno = 1;
+    double pi = 3, raggio;
     // Stampo i vari metodi disponibili
     printf("Con che metodo vuoi calcolare il pi greco?\n");
     printf("1. Formula di Leibniz\n");
@@ -39,48 +39,34 @@ int main(int argc, char *argv[])
         printf("\nInserisci il numero di volte di esecuzione del ciclo: ");
         scanf("%d", &iterazioni);
     } while (iterazioni < 1);
+    // Inizializzo le variabili
     if (scelta == 1)
     {
-        // Formula di Leibniz
-        // pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - ...
-        for (int i = 0; i < iterazioni; i++)
-        {
-            // Alterno somma e sottrazione
-            if (i % 2 == 0)
-            {
-                // Somma
-                pi += 4 / (double)(2 * i + 1);
-            }
-            else
-            {
-                // Sottrazione
-                pi -= 4 / (double)(2 * i + 1);
-            }
-        }
+        n = 4;
+        pi = 4;
+        d = 3;
+        segno = -1;
     }
-    else
+    // Calcolo il pi greco
+    for (int i = 1; i < iterazioni; i++)
     {
-        // Formula di Nilakantha
-        // pi = 3 + 1/(1*2*3) - 1/(2*3*5) + 1/(3*4*7) - 1/(4*5*9)
-        pi = 3;
-        for (int i = 1; i < iterazioni; i++)
+        if (scelta == 2)
         {
-            // Alterno somma e sottrazione
-            if (i % 2 == 1)
-            {
-                // Somma
-                pi += (double)1 / (i * (i + 1) * (2 * i + 1));
-            }
-            else
-            {
-                // Sottrazione
-                pi -= (double)1 / (i * (i + 1) * (2 * i + 1));
-            }
+            d = i * (i + 1) * (2 * i + 1);
         }
+        pi += segno * n / (double)d;
+        segno *= -1;
+        d += 2;
     }
-    printf("Il valore del pi greco calcolato è: %lf\n\n", pi);
-    printf("Inserisci il raggio del cerchio: ");
-    scanf("%lf", &raggio);
+    // Stampo il valore approsimativo del pi greco
+    printf("Il valore del pi greco calcolato è: %.15lf\n\n", pi);
+    // Chiedo il raggio del cerchio
+    do
+    {
+        printf("Inserisci il raggio del cerchio: ");
+        scanf("%lf", &raggio);
+    } while (raggio <= 0);
+    // Stampo il perimetro e l'area
     printf("Il valore dell'area è %.2lf\n", pi * raggio * raggio);
     printf("Il valore della circonferenza è %.2lf\n\n", 2 * pi * raggio);
     return 0;
