431bcfa1632edee2c78c5f2c93654297eda3eb6d
Matteo <matteo.fuso@iisviolamarchesini.edu.it>
2023-10-12 09:24:51 +0200

    Array puntatori, fopen()

--- Home/4/Ripasso/arrayDiPuntatori.c
+++ Home/4/Ripasso/arrayDiPuntatori.c
@@ -22,27 +22,37 @@ typedef struct
     float media;
 } student_t;
 
+void Inserimento(student_t *studente, int i)
+{
+    printf("Inserisci il nome del %d studente: ", i + 1);
+    scanf("%s", studente->nome);
+    printf("Inserisci il cognome del %d studente: ", i + 1);
+    scanf("%s", studente->cognome);
+    printf("Inserisci la media del %d studente: ", i + 1);
+    scanf("%f", &studente->media);
+    printf("\n");
+}
+
+void PrintArray(student_t *studenti[])
+{
+    for (int i = 0; i < STUDENTI; i++)
+    {
+        printf("Nome: %s, Cognome: %s, Media: %.2lf\n", studenti[i]->nome, studenti[i]->cognome, studenti[i]->media);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     student_t *studenti[STUDENTI];
     // Alloco la memoria e assegno i valori nella struct
     for (int i = 0; i < STUDENTI; i++)
     {
-        studenti[i] = malloc(sizeof(student_t));
-        printf("Inserisci il nome del %d studente: ", i + 1);
-        scanf("%s", studenti[i]->nome);
-        printf("Inserisci il cognome del %d studente: ", i + 1);
-        scanf("%s", studenti[i]->cognome);
-        printf("Inserisci la media del %d studente: ", i + 1);
-        scanf("%f", &studenti[i]->media);
-        printf("\n");
+        studenti[i] = (student_t *)malloc(sizeof(student_t));
+        Inserimento(studenti[i], i);
     }
     // Stampo i valori inseriti
     printf("Ecco gli studenti inseriti:\n");
-    for (int i = 0; i < STUDENTI; i++)
-    {
-        printf("Nome: %s, Cognome: %s, Media: %.2lf\n", studenti[i]->nome, studenti[i]->cognome, studenti[i]->media);
-    }
+    PrintArray(studenti);
     // Stamppo il contenuto dell'array
     printf("\nEcco il continuto dell'array:\n");
     for (int i = 0; i < STUDENTI; i++)
---
+++ Lab/4/Ripasso/files.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    file = fopen("file.txt", "r");
+    
+    return 0;
+}
