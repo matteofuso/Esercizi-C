85aa18ff25c295742d264d20b8d2547d67fb7f2d
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-05-14 18:23:41 +0200

    File copy with mutex

--- Classe 4/Programmazione Concorrente/ringBuffer.c
+++ Classe 4/Programmazione Concorrente/ringBuffer.c
@@ -14,6 +14,8 @@ typedef struct
 Block buffer[BLOCKS];
 int readPosition = 0;
 int writePosition = 0;
+pthread_cond_t condFill, condEmpty;
+pthread_mutex_t critical;
 
 void *Lettura(void *args)
 {
@@ -21,9 +23,15 @@ void *Lettura(void *args)
 
     do
     {
+        pthread_mutex_lock(&critical);
         // Aspetto il thread di scrittura
-        while (readPosition == writePosition + BLOCKS);
+        if (readPosition == writePosition + BLOCKS)
+        {
+            pthread_cond_wait(&condFill, &critical);
+        }
         buffer[readPosition % BLOCKS].n = fread(buffer[readPosition % BLOCKS].buffer, sizeof(char), BUFFER_SIZE, file);
+        pthread_cond_signal(&condEmpty);
+        pthread_mutex_unlock(&critical);
     } while (buffer[(readPosition++) % BLOCKS].n > 0);
 
     pthread_exit(NULL);
@@ -35,9 +43,15 @@ void *Scrittura(void *args)
 
     do
     {
+        pthread_mutex_lock(&critical);
         // Aspetto che ci sia il dato
-        while (readPosition == writePosition);
+        if (readPosition == writePosition)
+        {
+            pthread_cond_wait(&condEmpty, &critical);
+        }
         fwrite(buffer[writePosition % BLOCKS].buffer, sizeof(char), buffer[writePosition % BLOCKS].n, file);
+        pthread_cond_signal(&condFill);
+        pthread_mutex_unlock(&critical);
     } while (buffer[(writePosition++) % BLOCKS].n > 0);
 
     pthread_exit(NULL);
@@ -66,14 +80,23 @@ int main(int argc, char *argv[])
     if (destinazione == NULL)
     {
         printf("Impossibile aprire il file di destinazione\n");
+        fclose(origine);
         return -1;
     }
 
-    // Creazione thread
+    // Creazione thread e sezioni critiche
+    pthread_mutex_init(&critical, NULL);
+    pthread_cond_init(&condFill, NULL);
+    pthread_cond_init(&condEmpty, NULL);
+    
     pthread_create(&lettura, NULL, &Lettura, origine);
     pthread_create(&scrittura, NULL, &Scrittura, destinazione);
     pthread_join(lettura, NULL);
     pthread_join(scrittura, NULL);
+
+    pthread_mutex_destroy(&critical);
+    pthread_cond_destroy(&condFill);
+    pthread_cond_destroy(&condEmpty);
 
     // Chiusura file
     fclose(origine);
