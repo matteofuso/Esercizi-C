d49d5dc3a741628cdaf3bf4467ca7c107d5cba90
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-09-26 15:15:34 +0200

    Fine esercizio ripasso stringhe

--- Classe 5/Ripasso/stringhe.c
+++ Classe 5/Ripasso/stringhe.c
@@ -21,9 +21,10 @@
 
 #define STRLEN 100
 
+// Controlla se la stringa contiene solo lettere
 bool onlyLetter(char *string)
 {
-    for (int i = 0; i < strlen(string); i++)
+    for (int i = 0; string[i] != '\0'; i++)
     {
         if (!isalpha(string[i]))
         {
@@ -33,10 +34,11 @@ bool onlyLetter(char *string)
     return true;
 }
 
+// Conta quante volte è presente una lettera
 int count(char *string, char letter)
 {
     int count = 0;
-    for (int i = 0; i < strlen(string); i++)
+    for (int i = 0; string[i] != '\0'; i++)
     {
         if (string[i] == letter)
         {
@@ -46,10 +48,11 @@ int count(char *string, char letter)
     return count;
 }
 
+// Crea due stringhe con le lettere di posizione pari e dispari
 void EvenOddString(char *string, char *stringEven, char *stringOdd)
 {
     int j = 0, k = 0;
-    for (int i = 0; i < strlen(string); i++)
+    for (int i = 0; string[i] != '\0'; i++)
     {
         if (i % 2 == 0)
         {
@@ -64,13 +67,56 @@ void EvenOddString(char *string, char *stringEven, char *stringOdd)
     stringOdd[k] = '\0';
 }
 
+// Conta le lettere presenti nella stringa
 void strToAlpha(char *string, char *alpha)
 {
-    for (int i = 0; i < strlen(string); i++)
+    for (int i = 0; string[i] != '\0'; i++)
     {
         if (isalpha(string[i]))
         {
             alpha[tolower(string[i]) - 'a']++;
+        }
+    }
+}
+
+// Controlla se la stringa contiene vocali
+bool isVowel(char letter)
+{
+    letter = tolower(letter);
+    return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+}
+
+// Confronta il numero di vocali e consonanti tra due stringhe
+void compareVowelsConsonants(const char *string1, const char *string2, int *outVowels, int *outConsonants)
+{
+    *outVowels = 0;
+    *outConsonants = 0;
+    for (int i = 0; string1[i] != '\0'; i++)
+    {
+        if (isalpha(string1[i]))
+        {
+            if (isVowel(string1[i]))
+            {
+                (*outVowels)++;
+            }
+            else
+            {
+                (*outConsonants)++;
+            }
+        }
+    }
+    for (int i = 0; string2[i] != '\0'; i++)
+    {
+        if (isalpha(string2[i]))
+        {
+            if (isVowel(string2[i]))
+            {
+                (*outVowels)--;
+            }
+            else
+            {
+                (*outConsonants)--;
+            }
         }
     }
 }
@@ -82,6 +128,7 @@ int main(int argc, char **argv)
     char alpha[26] = {0}, alpha2[26] = {0};
     char letter;
     int times;
+    int vowels, consonants;
     // Chiedo di inserire la prima stringa
     printf("Inserisci una stringa: ");
     scanf("%s", string);
@@ -123,7 +170,7 @@ int main(int argc, char **argv)
     {
         printf("La prima stringa è più lunga della seconda\n");
     }
-    else if (strcmp(string, string2) < 0)
+    else if (strlen(string) < strlen(string2))
     {
         printf("La prima stringa è più corta della seconda\n");
     }
@@ -139,6 +186,32 @@ int main(int argc, char **argv)
         {
             printf("La lettera %c è presente in entrambe le stringhe, nella prima %d volte, nella seconda %d volte\n", i + 'a', alpha[i], alpha2[i]);
         }
+    }
+    // Stringa con più vocali e consonanti
+    compareVowelsConsonants(string, string2, &vowels, &consonants);
+    if (vowels > 0)
+    {
+        printf("La prima stringa contiene più vocali\n");
+    }
+    else if (vowels < 0)
+    {
+        printf("La seconda stringa contiene più vocali\n");
+    }
+    else
+    {
+        printf("Le stringhe contengono lo stesso numero di vocali\n");
+    }
+    if (consonants > 0)
+    {
+        printf("La prima stringa contiene più consonanti\n");
+    }
+    else if (consonants < 0)
+    {
+        printf("La seconda stringa contiene più consonanti\n");
+    }
+    else
+    {
+        printf("Le stringhe contengono lo stesso numero di consonanti\n");
     }
     return 0;
 }
