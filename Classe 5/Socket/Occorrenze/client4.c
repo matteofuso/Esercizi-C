132ff1a21b20dec4d4e5d936530adc8140e9a92a
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-10-22 18:20:42 +0200

    Esercizio Occorrenze socket

--- Classe 5/Socket/Occorrenze/client4.c
+++ Classe 5/Socket/Occorrenze/client4.c
@@ -11,11 +11,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 #define SRV_PORT 3333
 #define BUFFERSIZE 128
 
+struct Richiesta
+{
+    char stringa[BUFFERSIZE];
+    char carattere;
+};
+
 int main(int argc, char *argv[])
 {
+    struct Richiesta richiesta;
+    struct sockaddr_in server;
+    int serverfd, clientfd, corrispondenze, socketlen = sizeof(struct sockaddr_in);
+    // Controllo argomenti
+    if (argc != 3)
+    {
+        printf("Errore nel numero di argomenti\n");
+        printf("Usa %s [stringa] [carattere]\n", argv[0]);
+        exit(-1);
+    }
+    // Creazione della struttura
+    strcpy(richiesta.stringa, argv[1]);
+    richiesta.carattere = argv[2][0];
+    // Creazione della socket
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = ntohl(INADDR_ANY);
+    server.sin_port = ntohs(SRV_PORT);
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd < 0)
+    {
+        printf("Errore nella creazione della socket\n");
+        exit(-2);
+    }
+    // Connessione al server
+    if (connect(serverfd, (struct sockaddr *)&server, socketlen) < 0)
+    {
+        printf("Errore nella connessione con il server\n");
+        exit(-3);
+    }
+    // Invio della richiesta
+    write(serverfd, &richiesta, sizeof(struct Richiesta));
+    read(serverfd, &corrispondenze, sizeof(int));
+    printf("La stringa contiene %d corrispondenze\n", corrispondenze);
+    // Chiusura
+    close(serverfd);
     return 0;
 }
--- Classe 5/Socket/Occorrenze/server4.c
+++ Classe 5/Socket/Occorrenze/server4.c
@@ -11,11 +11,67 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 #define PORT 3333
 #define BUFFERSIZE 128
 
+struct Richiesta
+{
+    char stringa[BUFFERSIZE];
+    char carattere;
+};
+
+int contaCorrispondenze(struct Richiesta richiesta)
+{
+    int count = 0, len = strlen(richiesta.stringa);
+    for (int i = 0; i < len; i++)
+    {
+        if (richiesta.stringa[i] == richiesta.carattere)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main(int argc, char *argv[])
 {
+    struct Richiesta richiesta;
+    struct sockaddr_in client, server;
+    int serverfd, clientfd, corrispondenze, socketlen = sizeof(struct sockaddr_in);
+    // Creazione della socket
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = ntohl(INADDR_ANY);
+    server.sin_port = ntohs(PORT);
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd < 0)
+    {
+        printf("Errore nella creazione della socket\n");
+        exit(-1);
+    }
+    // Creazione del canale
+    bind(serverfd, (struct sockaddr *)&server, socketlen);
+    listen(serverfd, 10);
+    printf("Server in ascolto alla porta %d\n\n", PORT);
+    // Accettazione delle connessioni
+    while (1)
+    {
+        clientfd = accept(serverfd, (struct sockaddr *)&client, &socketlen);
+        if (clientfd < 0)
+        {
+            printf("Tentativo di connessione interrotto\n");
+            continue;
+        }
+        printf("Connessione accettata\n");
+        // Elaborazione della richiesta
+        read(clientfd, &richiesta, sizeof(struct Richiesta));
+        printf("Ricevuta la stringa \"%s\" e il carattere \"%c\"\n", richiesta.stringa, richiesta.carattere);
+        corrispondenze = contaCorrispondenze(richiesta);
+        write(clientfd, &corrispondenze, sizeof(int));
+        printf("Inviato il numero di occorrenze (%d), chiusura connessione\n\n", corrispondenze);
+        close(clientfd);
+    }
+    close(serverfd);
     return 0;
 }
